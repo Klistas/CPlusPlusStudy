0688fc6e630788dd806fdcdddb0c0def9090e8cf
Klistas <fukoyjh@gmail.com>
2022-05-04 17:40:36 +0900

    coommit

---
+++ ConsoleApplication2/BJ/BJ.c
@@ -0,0 +1,78 @@
+﻿#include <stdio.h>
+#include <assert.h>
+
+//int notSelfNum(int n)
+//{
+//	int selfNum = (n + (n / 1000) + ((n % 1000) / 100)
+//		+ ((n % 100) / 10) + (n % 10));
+//	return selfNum;
+//}
+
+
+
+
+int main(void)
+{
+	FILE* fp = NULL;
+	/*if (0 != fopen_s(&fp, "Metaverse.txt", "w"))
+
+	{
+		printf("오류 발생함.");
+			return 1;
+	}*/
+	if (0 != fopen_s(&fp, "Metaverse1", "wb"))
+	{
+		printf("오류 발생함.");
+
+		return 1;
+	}
+		
+
+	struct Student
+	{
+		int Age;
+		enum { A, B, O, AB } BloodType;
+		char Name[24];
+	};
+
+	struct Student s = { 28, A, "YongJoonHeon" };
+	
+	if (1 != fwrite(&s, sizeof(s), 1, fp))
+	{
+		printf("오류 발생함.");
+		fclose(fp);
+		return 1;
+	}
+	
+
+	fclose(fp);
+
+	if (0 != fopen_s(&fp, "Metaverse1", "rb"))
+	{
+		printf("오류 발생함.");
+
+		fclose(fp);
+
+		return 1;
+	}
+
+	struct Student s2 = { 0 };
+	if (1 != fread(&s2, sizeof(s2), 1, fp))
+	{
+		printf("오류 발생함.");
+
+		fclose(fp);
+
+		return 1;
+	}
+
+	printf("나이 : %d, 혈액형 : %d, 이름 : %s\n", s2.Age, s2.BloodType, s2.Name);
+
+	fclose(fp);
+
+
+
+
+
+	return 0;
+}
--- ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.c
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.c
@@ -1,43 +1,84 @@
 ﻿#include <stdio.h>
 #include <stdarg.h>
 
+
+void putNumber (int n)
+{
+	const int checkNegative = n & (1 << 31);
+	if(checkNegative)
+	{
+		putchar('-');
+		n -= 1;
+		n = ~n;
+	}
+	
+	int numString[16] = { 0 } ;
+	int numCount = 0;
+	
+	while (n != 0)
+	{
+		int digit = n % 10;
+		numString[numCount] = digit + '0';
+		numCount++;
+		n /= 10;
+	} 
+
+	for (int i = numCount - 1; i >= 0; i--)
+	{
+		putchar(numString[i]);
+	}
+}
+
+
 void myprintf(const char* format, ...)
 {
 	va_list args;
 	va_start(args, format);
-	int d[1000];
+	
 	while(*format != '\0')
 	{
-		putchar(*format);
+		
 		
 		if (*format == '%')
 		{
 			
-			format++;
 			
+			format++;
+			if (*format == '\0')
+			{
+				break;
+			}
 			
 			if (*format == 'd')
 			{
-				int count = va_arg(args, int);
-				putchar(d[count]);
+				int d = va_arg(args, int);
+				putNumber(d);
+				format++;
 			}
 
 			else if (*format == 'c')
 			{
 				char c = va_arg(args, char);
 				putchar(c);
-			}
-
-			else if (*format == 's')
-			{
-				const char *s =  va_arg(args, const char*) ;
-				putchar(*s);
+				format++;
 			}
 			
+			else if (*format == 's')
+			{
+				char* s =  va_arg(args, char*) ;
+				for (int i = 0; s[i] != '\0'; i++)
+				{
+					putchar(s[i]);
+					
+				}
+				format++;
+			}
+		
 
 		}
 		
 			
+		putchar(*format);
 		format++;
 		
 		
@@ -49,6 +90,6 @@ void myprintf(const char* format, ...)
 
 int main(void)
 {
-	myprintf("Hello My Printf : %d, %c, %s", 40, 'H', "Hello");
+	myprintf("Hello My Printf : %d, %c, %s", -40, 'H', "Hello");
 	return 0;
 }
